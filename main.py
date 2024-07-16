8adbef4e604ae6236bd700af519dd07db7ab8314
Rigel Di Scala <zedr@zedr.com>
2024-07-16 16:12:07 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+from typing import Iterable, Generator
+from functools import cached_property
+
+
+def _make_uniq(words: Iterable[str]) -> list[str]:
+    seq = []
+    for word in words:
+        if word not in seq:
+            seq.append(word)
+        else:
+            raise ValueError(f"Repeated word: {word}")
+    return seq
+
+
+class OneHotEncoding:
+    def __init__(self, size: int, words_ns: dict[int, str]):
+        if max_ := max(words_ns) > size:
+            raise ValueError(f"Out of bounds: {size} < {max_}")
+        if min(words_ns) < 0:
+            raise ValueError(f"Words cannot have a negative encoding")
+
+        self._size = size
+        self._words_ns = words_ns
+
+    def __len__(self) -> int:
+        return len(self._words_ns)
+
+    def __iter__(self):
+        return iter(self.as_seq())
+
+    def as_seq(self) -> Generator[int, None, None]:
+        for idx in range(self._size):
+            yield 1 if self._words_ns.get(idx) else 0
+
+
+class OneHotEncoder:
+    """One hot encoder using a list of given words"""
+
+    def __init__(self, words: Iterable[str]):
+        self._words = _make_uniq(words)
+
+    @cached_property
+    def length(self):
+        return len(self._words)
+
+    def get_index(self, word: str) -> int:
+        return self._words.index(word)
+
+    def get_word(self, idx: int) -> str:
+        return self._words[idx]
+
+    def encode(self, *words: str) -> Generator[OneHotEncoding, None, None]:
+        for word in words:
+            yield OneHotEncoding(self.length, {self.get_index(word): word})
+
+    @classmethod
+    def from_file(cls, path: str) -> "OneHotEncoder":
+        with open(path) as fd:
+            return cls(line.strip() for line in fd)
---
+++ tests.py
@@ -0,0 +1,31 @@
+from main import OneHotEncoder, OneHotEncoding
+
+
+def test_enconding_1():
+    enc = list(OneHotEncoding(5, {0: "find"}))
+    assert enc == [1, 0, 0, 0, 0]
+
+
+def test_encoder_api():
+    words = ["files", "find", "my"]
+    enc = OneHotEncoder(words)
+    for word in words:
+        idx = enc.get_index(word)
+        assert word == enc.get_word(idx)
+
+
+def test_encoder_encoding():
+    words = ["files", "find", "my", "mind"]
+    enc = OneHotEncoder(words)
+    encoding = enc.encode("find", "my", "files")
+    encoding = [list(item) for item in encoding]
+    assert encoding == [
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [1, 0, 0, 0]
+    ]
+
+
+def tut_test_1():
+    enc = OneHotEncoder(["files", "find", "my"])
+    assert [1, 0, 0] == enc.encode("find", "my", "files")
